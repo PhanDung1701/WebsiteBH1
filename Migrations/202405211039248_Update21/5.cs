c7717d18d918386d59cd3efeef40dcfbd2f8895b
PhanDung1701 <141790127+PhanDung1701@users.noreply.github.com>
2024-05-21 17:47:09 +0700

    Update21/5

---
+++ Migrations/202405211039248_Update21/5.cs
@@ -0,0 +1,18 @@
+﻿namespace WebsiteBH.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Update215 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Subscribe", "Email", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Subscribe", "Email", c => c.String(nullable: false));
+        }
+    }
+}
